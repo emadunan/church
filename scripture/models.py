5dcb78d5f294202ac18c2858a4610017f4431a10
emadunan <emadunan@gmail.com>
2021-05-29 00:26:15 +0200

    BookMeta, Character models added

--- scripture/models.py
+++ scripture/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Scripture models.
 
@@ -8,21 +9,39 @@ class User(AbstractUser):
     pass
 
 
+class Character(models.Model):
+    name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    fullname = models.TextField()
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(969)])
+    about_me = models.TextField()
+    is_writter = models.BooleanField()
+
+
+class BooKMeta(models.Model):
+    written_from = models.DateField(auto_now=False, auto_now_add=False)
+    written_to = models.DateField(auto_now=False, auto_now_add=False)
+    written_in = models.TextField()
+    written_by = models.ManyToManyField(Character)
+
+
 class Book(models.Model):
+    name = models.CharField(max_length=30)
     number = models.IntegerField()
     symbol = models.CharField(max_length=3)
     title = models.CharField(max_length=33)
-    titlelong = models.CharField(max_length=55)
-    chapters = models.ForeignKey('Chapter', on_delete=models.CASCADE)
+    titlelong = models.CharField(max_length=99)
+    book_meta = models.OneToOneField(BooKMeta, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f"({self.number}) {self.title}"
+        return f"({self.number}) {self.name}"
 
 
 class Chapter(models.Model):
     number = models.IntegerField()
     title = models.CharField(max_length=55)
-    verses = models.ForeignKey('Verse', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"({self.number}) {self.title}"
@@ -32,6 +51,7 @@ class Verse(models.Model):
     number = models.IntegerField()
     text = models.TextField()
     textf = models.TextField()
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"({self.number}) {self.textf}"
