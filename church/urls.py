317b4ef3b945e31be95ac022f91c296c1374e5a7
emadunan <emadunan@gmail.com>
2021-06-13 12:16:48 +0200

    bootstrap5 name changed

--- church/urls.py
+++ church/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('scripture.urls'))
+    path('', include('scripture.urls'))
 ]
