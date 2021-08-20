1b71a7a2a24956f647c8517f7c00f3567024ecd0
emadunan <emadunan@gmail.com>
2021-08-20 22:15:58 +0200

    highlight search results trial

--- scripture/static/scripture/js/search.js
+++ scripture/static/scripture/js/search.js
@@ -1,7 +1,31 @@
-// id_search-books__heading
-
-document.addEventListener('DOMContentLoaded', function() {
-    const mainStr = document.getElementById('id_search-criteria').textContent;
-    const strArr = mainStr.split(' ');
-    console.log(mainStr);
+document.addEventListener("DOMContentLoaded", function () {
+//   const criteriaEl = document.getElementById("id_search-criteria");
+//   if (criteriaEl) {
+//     const mainStr = criteriaEl.textContent;
+//     const strArr = mainStr.split(" ");
+//     console.log(strArr);
+//     strArr.forEach(element => {
+//         highlight(element);
+//         console.log(element);
+//     });
+//   }
 });
+
+// لا تعمل بسبب التشكيل
+function highlight(text) {
+  var inputTexts = document.querySelectorAll(".cc_verse-textf");
+  inputTexts.forEach(inputText => {
+    console.log(inputText);
+    var innerHTML = inputText.innerHTML;
+    var index = innerHTML.indexOf(text);
+    if (index >= 0) {
+      innerHTML =
+        innerHTML.substring(0, index) +
+        "<span class='highlight'>" +
+        innerHTML.substring(index, index + text.length) +
+        "</span>" +
+        innerHTML.substring(index + text.length);
+      inputText.innerHTML = innerHTML;
+    }
+  });
+}
